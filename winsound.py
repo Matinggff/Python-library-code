c27b0344d07879dcd8ee551cc9d7f8d8f7658069
Matin Salehi <119169794+Matinggff@users.noreply.github.com>
2023-11-17 21:34:40 +0330

    add winsound

---
+++ winsound.py
@@ -0,0 +1,12 @@
+import winsound
+from random import randint
+
+def create_music():
+    sum_duration = 0
+    while True:
+        duration = randint(200, 1000)
+        sum_duration += duration
+        winsound.Beep(randint(50, 2000), duration)
+        if sum_duration >= 6000:
+            break
+create_music()
